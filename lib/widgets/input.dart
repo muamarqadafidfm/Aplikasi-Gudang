a1a8d45f093b6c3b94671d3667ef294d7c8f996a
dafadeavu <dafadejavu0@gmail.com>
2022-06-06 12:00:04 +0700

    mengubah button 1 & 2, warna

--- lib/widgets/input.dart
+++ lib/widgets/input.dart
@@ -1,59 +1,58 @@
 import 'package:flutter/material.dart';
-  
+
 class Input extends StatefulWidget {
   const Input({Key? key}) : super(key: key);
-  
-  @override  
-  _State createState() => _State();  
-}  
-  
-class _State extends State<Input> {  
-  @override  
-  Widget build(BuildContext context) {  
-    return Scaffold(  
-        appBar: AppBar(  
-          title: const Text('CheckBarang'), 
-          backgroundColor: Colors.black, 
-        ),  
-        body: Padding(  
-            padding: const EdgeInsets.all(15),  
-            child: Column(  
-              children: <Widget>[  
-                const Padding(  
-                  padding: EdgeInsets.all(15),  
-                  child: TextField(  
-                    decoration: InputDecoration(  
-                      border: OutlineInputBorder(),  
-                      labelText: 'Input 1',  
-                      hintText: 'Data', 
-                    ),  
-                  ),  
-                ),  
-                const Padding(  
-                  padding: EdgeInsets.all(15),  
-                  child: TextField(  
-                    decoration: InputDecoration(  
-                      border: OutlineInputBorder(),  
-                      labelText: 'Input 2',  
-                      hintText: 'Data',  
-                    ),  
-                  ),  
-                ),  
+
+  @override
+  _State createState() => _State();
+}
+
+class _State extends State<Input> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('CheckBarang'),
+          backgroundColor: Color.fromARGB(255, 255, 120, 120),
+        ),
+        body: Padding(
+            padding: const EdgeInsets.all(15),
+            child: Column(
+              children: <Widget>[
+                const Padding(
+                  padding: EdgeInsets.all(15),
+                  child: TextField(
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Input 1',
+                      hintText: 'Data',
+                    ),
+                  ),
+                ),
+                const Padding(
+                  padding: EdgeInsets.all(15),
+                  child: TextField(
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Input 2',
+                      hintText: 'Data',
+                    ),
+                  ),
+                ),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                   children: [
-                    RaisedButton(  
-                      textColor: Colors.white,  
-                      color: Colors.black,  
-                      child: const Text('Button 1'),  
-                      onPressed: (){},  
+                    RaisedButton(
+                      textColor: Colors.white,
+                      color: Color.fromARGB(255, 255, 111, 111),
+                      child: const Text('Inputkan'),
+                      onPressed: () {},
                     ),
-
-                    RaisedButton(  
-                      textColor: Colors.white,  
-                      color: Colors.black,  
-                      child: const Text('Button 2'),  
-                      onPressed: (){},  
+                    RaisedButton(
+                      textColor: Colors.white,
+                      color: Color.fromARGB(255, 255, 122, 122),
+                      child: const Text('Scan'),
+                      onPressed: () {},
                     ),
                   ],
                 ),
@@ -63,14 +62,12 @@ class _State extends State<Input> {
                   decoration: BoxDecoration(
                     borderRadius: BorderRadius.circular(20),
                     border: Border.all(
-                      color: Colors.black,
+                      color: Color.fromARGB(255, 255, 108, 108),
                       width: 2,
                     ),
                   ),
-                ), 
-              ],  
-            )  
-        )  
-    );  
-  }  
-}  
+                ),
+              ],
+            )));
+  }
+}
